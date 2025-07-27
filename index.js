cf7b48792eb76d47a6129ef80c745da2514b2657
Mazin <code.mazin.m@gmail.com>
2025-07-27 10:20:04 +1000

    test passed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const input = document.getElementById('button');
-
-function clickAlert() {
-  alert('I was clicked!');
+function addingEventListener() {
+  const input = document.getElementById("button");
+  input.addEventListener("click", function () {
+    alert("I was clicked!");
+  });
 }
-
-input.addEventListener('click', clickAlert);
